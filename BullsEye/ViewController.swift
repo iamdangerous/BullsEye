63902412f3c6d60c35d622a116f37ca51ba86702
Rahul Lohra <rahullohra@Rahuls-MacBook-Air.local>
2017-11-15 22:44:04 +0530

    First commit

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentValue:Int = 50
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +21,25 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func sliderMOved(_ sender: UISlider) {
+    }
+    
+    
+    @IBAction func showHelloAlert(){
+        
+        let message = "The value of the slider is: \(currentValue)"
+        
+        let alert = UIAlertController(title: "Hello world", message: message, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
 
 
 }
