eb16f9f2e70ae8f9f648de5cc8ce826ea43252c4
Rahul Lohra <rahullohra@Rahuls-MacBook-Air.local>
2017-11-17 23:56:13 +0530

    add closure

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -12,20 +12,34 @@ class ViewController: UIViewController {
     
     var currentValue:Int = 50
     @IBOutlet weak var slider:UISlider!
-    var targetValue:Int = 0
-
+    @IBOutlet weak var targetLabel:UILabel!
+    @IBOutlet weak var scoreLabel:UILabel!
+    @IBOutlet weak var roundLabel:UILabel!
+    var targetValue = 0
+    var score = 0
+    var currentRound = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         currentValue = lroundf(slider.value)
         startNewRound()
     }
     
+    func updateLabels(){
+        slider.value = Float(currentValue)
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(currentRound)
+
+    }
+    
     func startNewRound(){
         targetValue = 1+Int(arc4random_uniform(100))
         currentValue = 50
-        slider.value = Float(currentValue)
+        currentRound += 1
+        updateLabels()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -37,21 +51,50 @@ class ViewController: UIViewController {
     
     @IBAction func showHelloAlert(){
         
-        let message = "The value of the slider is: \(currentValue)"+"\n Target value is \(targetValue)"
+        let difference:Int = abs(targetValue - currentValue)
+        var points = 100 - difference
         
-        let alert = UIAlertController(title: "Hello world", message: message, preferredStyle: .alert)
+        //set title based on score
         
-        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        let title:String
+        if(difference == 0){
+            title = "Perfect!"
+            points += 100
+        }else if difference < 5{
+            title = "You almost had it!"
+            
+            if difference == 1{
+                points += 50
+            }
+        }else if difference < 10{
+            title = "Pretty good!"
+        }else {
+            title = "Not even close..."
+        }
+        
+        score += points
+ 
+        
+//        let message = "The value of the slider is: \(currentValue)"+"\n Target value is \(targetValue)"+"\n The difference is \(difference)"
+       
+        let message = "You scored \(points)"
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: {
+            action in
+            self.startNewRound()
+        })
         
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
         
-        startNewRound()
+    
     }
     
     
-
-
+    
+    
 }
 
