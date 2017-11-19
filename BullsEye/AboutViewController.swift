5336833e7dd9d84d459b8fdd42dab87c93d03409
Rahul Lohra <tgunix@gmail.com>
2017-11-19 14:02:55 +0530

    add webview

--- BullsEye/AboutViewController.swift
+++ BullsEye/AboutViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class AboutViewController: UIViewController {
-    @IBOutlet weak var gameDetail: UITextView!
+    
+    @IBOutlet weak var webView: UIWebView!
     
     @IBAction func close(){
         dismiss(animated: true, completion: nil)
@@ -18,7 +19,18 @@ class AboutViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        gameDetail.text = "*** Bull's Eye *** \n\nWelcome to the awesome game of Bull'e Eye where you can win points and fame by dragging a slider. \n\nYour goal is to place the slider as close as possible to the target value. The closer you are the more points you get. Enjoy!"
+        setupWebView()
+        
+    }
+    
+    func setupWebView(){
+        
+        if let url = Bundle.main.url(forResource: "BullsEye", withExtension: "html"){
+            if let htmlData = try?Data(contentsOf:url){
+                 let baseUrl = URL(fileURLWithPath: Bundle.main.bundlePath)
+                webView.load(htmlData, mimeType: "text/html", textEncodingName: "UTF-8", baseURL: baseUrl)
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
