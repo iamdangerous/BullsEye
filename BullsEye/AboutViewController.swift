bbaee9913e991aca60a12a969309ab5a054bb5b0
Rahul Lohra <tgunix@gmail.com>
2017-11-19 13:49:19 +0530

    add styling in info screen

--- BullsEye/AboutViewController.swift
+++ BullsEye/AboutViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class AboutViewController: UIViewController {
+    @IBOutlet weak var gameDetail: UITextView!
     
     @IBAction func close(){
         dismiss(animated: true, completion: nil)
@@ -17,7 +18,7 @@ class AboutViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        gameDetail.text = "*** Bull's Eye *** \n\nWelcome to the awesome game of Bull'e Eye where you can win points and fame by dragging a slider. \n\nYour goal is to place the slider as close as possible to the target value. The closer you are the more points you get. Enjoy!"
     }
 
     override func didReceiveMemoryWarning() {
